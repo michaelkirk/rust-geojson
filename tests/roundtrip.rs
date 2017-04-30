a40b4a52dec6944077c27d0f19bdde01c1356a7d
Michael Kirk <michael.code@endoftheworl.de>
2017-04-30 13:44:15 -0400

    wip roundtrip test

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -1,4 +1,5 @@
 extern crate geojson;
+extern crate serde_json;
 
 #[cfg(test)]
 mod tests {
@@ -8,26 +9,46 @@ mod tests {
 		use std::io::prelude::*;
 		use std::fs;
 		use geojson::GeoJson;
+        use serde_json::Value;
+        use serde_json;
 
 		let fixture_dir_path = "tests/fixtures/good";
 
 		for dir_entry in fs::read_dir(fixture_dir_path).unwrap() {
-			// to see printed output: `cargo test -- --nocapture`
 			let path = dir_entry.unwrap().path();
 			let path_str = path.to_str().unwrap();
-			println!("Name: {}", &path_str);
+
+			//println!("Name: {}", &path_str);
+
 			let mut file = File::open(&path).unwrap();
 			let mut file_contents = String::new();
 			file.read_to_string(&mut file_contents);
 
 			//println!("Contents: {}", &file_contents);
-            match file_contents.parse::<GeoJson>() {
-                Ok(geojson) => println!(">> success! file: {}", &path_str),
-                Err(error) => {
-                    println!(">> failure! file: {} error: {}", &path_str, &error); 
-                    assert!(false)
-                }
-            };
+
+            let geojson_result = file_contents.parse::<GeoJson>();
+
+            // match geojson_result {
+            //     Ok(geojson) => println!(">> parse success! file: {}", &path_str),
+            //     Err(error) => {
+            //         println!(">> parse failure! file: {} error: {}", &path_str, &error);
+            //         // FIXME actually fail here.
+            //         // TODO wrap in custom error, attaching failing file
+            //         //assert!(false)
+            //     }
+            // };
+
+            let geojson_string = geojson_result.unwrap().to_string();
+
+            let original_json: serde_json::Value = serde_json::from_str(&file_contents).unwrap();
+            let roundtrip_json: serde_json::Value = serde_json::from_str(&geojson_string).unwrap();
+
+            if (original_json == roundtrip_json) {
+                println!(">> roundtrip success! file: {}", &path_str);
+            } else {
+                println!(">> roundtrip failure! file: {}", &path_str);
+            }
+            //assert_eq!(original_json, roundtrip_json);
 		}
     }
 }
